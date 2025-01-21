6a28cec8c065737866f445e900d09f288ebfe5ab
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2025-01-21 10:27:35 +0100

    updated simplelogger

--- src/test/java/setup/BaseSetUp.java
+++ src/test/java/setup/BaseSetUp.java
@@ -1,6 +1,9 @@
 package setup;
 
 import context.ContextStore;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +19,10 @@ public class BaseSetUp {
     @BeforeAll
     public static void loadPropertiesForStaticVariables() {
         ContextStore.loadPropertiesFile();
+        RestAssured.filters(
+                new RequestLoggingFilter(),
+                new ResponseLoggingFilter()
+        );
     }
 
     @BeforeEach
